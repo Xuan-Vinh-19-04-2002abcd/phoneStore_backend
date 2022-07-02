7d954b56ec907474640fc19c02ec25fef51c7b65
Xuan-Vinh-19-04-2002abcd <88564890+Xuan-Vinh-19-04-2002abcd@users.noreply.github.com>
2022-07-02 09:37:11 +0700

    first commit

---
+++ UpdateData/alldata.js
@@ -0,0 +1,13 @@
+module.exports = () => ({
+  data: [
+    {
+      iphone: require("./iphone.json"),
+    },
+    {
+      laptop: require("./laptop.json"),
+    },
+    {
+      tablet: require("./tablet.json"),
+    }
+  ],
+});
